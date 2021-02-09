b81683a86174f7706a8cdb5aff7ef8fbd7d8617c
Shashi Singh <singh.shashishekhar@gmail.com>
2021-02-09 17:14:25 +0530

    Added Google Analytics and Crisp Chat

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,5 +19,31 @@ module.exports = {
       },
     },
     "gatsby-transformer-remark",
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: [
+          "UA-77981996-1",
+        ],
+        gtagConfig: {
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        pluginConfig: {
+          head: true,
+          respectDNT: true,
+          exclude: [],
+        }
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-crisp-chat',
+      options: {
+        websiteId: 'd5d3949c-b235-4d0b-990c-6485ae14053b',
+        enableDuringDevelop: false, // Optional. Disables Crisp Chat during gatsby develop. Defaults to true.
+        defer: true, // Optional. Sets the Crisp loading script to defer instead of async. Defaults to false.
+        enableImprovedAccessibility: false // Optional. Sets aria-label attribute on pop-up icon for screen readers. Defaults to true.
+      },
+    }
   ],
 }
