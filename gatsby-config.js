58b6c86abb2806d0bfbf6433a52af5795acc0c12
Shashi Singh <singh.shashishekhar@gmail.com>
2021-09-08 05:23:16 +0530

    Also including the gtag as it may be required to use the Komito plugin in Tag Manager

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,11 +20,28 @@ module.exports = {
     },
     "gatsby-transformer-remark",
     {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: [
+          "UA-77981996-1",
+        ],
+        gtagConfig: {
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        pluginConfig: {
+          head: true,
+          respectDNT: true,
+          exclude: [],
+        }
+      }
+    },
+    {
       resolve: `gatsby-plugin-google-tagmanager`,
       options: {
         id: "GTM-KRMM7BN",
-        includeInDevelopment: false,
-      }
+        includeInDevelopment: false
+      },
     },
     {
       resolve: 'gatsby-plugin-crisp-chat',
