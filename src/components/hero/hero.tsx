0747ec474f562efbd341da51dedf1fa7758624ac
Shashi Singh <singh.shashishekhar@gmail.com>
2021-09-08 00:10:52 +0530

    feat: Track click of outbound link

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -5,6 +5,7 @@ import { Customizations } from '@fluentui/react/lib/Utilities';
 import { themes } from '../../common/themes'
 import { FontSizes } from '@uifabric/fluent-theme/lib/fluent/FluentType';
 import { Stack, Text, PrimaryButton } from '@fluentui/react';
+import { OutboundLink } from 'gatsby-plugin-google-gtag';
 
 
 const HeroContainer: React.FunctionComponent = () =>  {
@@ -17,12 +18,12 @@ const HeroContainer: React.FunctionComponent = () =>  {
             </div>
             <div className="hero_cta">
                 <div className="myPrimaryButton">
-                    <a href="https://forms.zohopublic.in/gettrici/form/TriciDownloads/formperma/a3H9zdJkx7u6ZrSvnNDeIltYE4L2s-LQiAWzAoQt7xY?referrername=gettrici">Request Access | Download</a>
+                    <OutboundLink href="https://forms.zohopublic.in/gettrici/form/TriciDownloads/formperma/a3H9zdJkx7u6ZrSvnNDeIltYE4L2s-LQiAWzAoQt7xY?referrername=gettrici">Request Access | Download</OutboundLink>
                 </div>
             </div>
             <p className="available-announcement">Desktop application available for MacOS<sup>*</sup> and Ubuntu. Coming Soon for Windows<sup>**</sup></p>
             </div>
-            <img src="images/TriciMainScreen.webp" alt="Trici Main Screen" className="mainscreenshot" />
+            {/* <img src="images/TriciMainScreen.webp" alt="Trici Main Screen" className="mainscreenshot" /> */}
         </div>
     )
 }
