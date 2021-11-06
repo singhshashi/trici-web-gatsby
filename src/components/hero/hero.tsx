99c2ffb0e4fe332811edb39a0585c7b1d063fd67
Shashi Singh <singh.shashishekhar@gmail.com>
2021-11-06 23:18:32 +0530

    feat: Added separate page for Apple Silicon build and added now taking the version from the query string params so I dont have to update the gatsby build for every new release.

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -118,7 +118,7 @@ const HeroContainer: React.FunctionComponent = () =>  {
                     <p className="byline">Record and recap your Focus Sessions and rebuild your lost train of thought in seconds!</p>
             </div>
             <div className="hero_cta">
-                <PrimaryButton text="Download | Request Access" onClick={toggleHideDialog} styles={heroButtonStyles} />
+                <PrimaryButton text="Download for Free" onClick={toggleHideDialog} styles={heroButtonStyles} iconProps={{ iconName: "Installation" }} />
                 <Dialog
                     hidden={hideDialog}
                     onDismiss={toggleHideDialog}
@@ -156,7 +156,7 @@ const HeroContainer: React.FunctionComponent = () =>  {
                     <OutboundLink href="https://forms.zohopublic.in/gettrici/form/TriciDownloads/formperma/a3H9zdJkx7u6ZrSvnNDeIltYE4L2s-LQiAWzAoQt7xY?referrername=gettrici">Request Access | Download</OutboundLink>
                 </div> */}
             </div>
-            <p className="available-announcement">Desktop application available for MacOS<sup>*</sup> and Ubuntu. Coming Soon for Windows<sup>**</sup></p>
+            <p className="available-announcement">Desktop application available for MacOS and Ubuntu. Coming Soon for Windows<sup>*</sup></p>
             </div>
             <img src="images/TriciHomeExplain.png" alt="Trici Main Screen" className="mainscreenshot" />
         </div>
---
+++ src/pages/donwloads/mac-apple-silicon.tsx
@@ -0,0 +1,51 @@
+import React from "react"
+import Layout from "../../components/layout/layout"
+import { Text, CompoundButton } from "@fluentui/react"
+import "../../templates/page.css"
+import "./downloads.css"
+
+const MacAppleSiliconDownload: React.FunctionComponent = () => {
+  function downloadInstaller(e){
+    console.log('Download Button was clicked');
+    const urlSearchParams = new URLSearchParams(window.location.search);
+    const ver = urlSearchParams.get('ver');
+    if (ver) {
+        var downloadUrl = `https://download.gettrici.com/darwin-x64/Trici-${ver}-arm64.dmg`;
+        window.open(downloadUrl, '_blank');
+    } else {
+        window.open('https://download.gettrici.com/darwin-x64/Trici-2.5.0-arm64.dmg');
+    }
+  }
+  return (
+    <Layout>
+      <div className="pageContainer">
+        <div className="donwloadContainer">
+          <div className="donwloadButtonContainer">
+            <img
+              alt="Mac"
+              src="https://cdn.gettrici.com/mac.svg"
+              className="macos-logo"
+            />
+            <CompoundButton
+              primary
+              secondaryText="MacOS 11.6+"
+              iconProps={{ iconName: "Download" }}
+              className="downloadButton"
+              onClick={downloadInstaller}
+            >
+              DOWNLOAD
+            </CompoundButton>
+          </div>
+          <div className="legalstuff">
+            <Text as="p" variant="small" block>By using Trici, you agree to its <a href="/terms">Terms of Use</a> and <a href="/privacy">privacy statement</a>.</Text>
+            <Text as="p" variant="small" block>
+              Mac and the Mac logo are trademarks of Apple Inc., registered in
+              the U.S. and other countries.
+            </Text>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  )
+}
+export default MacAppleSiliconDownload
