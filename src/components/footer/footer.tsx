158edbf277985578ebbef115e11c318dd1809701
Shashi Singh <singh.shashishekhar@gmail.com>
2022-05-31 18:05:39 +0530

    fix: Added links to the footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { graphql, useStaticQuery } from "gatsby"
 import { ThemeProvider } from "@fluentui/react-theme-provider"
-import { Text } from "@fluentui/react"
+import { Text, Link } from "@fluentui/react"
 import { Stack } from "@fluentui/react/lib/Stack"
 import { themes } from "../../common/themes"
 import "./footer.css"
@@ -37,11 +37,22 @@ const Footer = () => {
           </Text>
         </div>
         <div className="links">
-          <Stack>
+          <Stack verticalAlign="space-evenly" verticalFill>
+          <Stack.Item>
+              <Stack tokens={{childrenGap: 15}}>
+                <Stack.Item>
+                  <Link href="/blog" theme={themes.themeTriciDark}>Blog</Link>
+                </Stack.Item>
+                <Stack.Item>
+                  <Link href="/docs" theme={themes.themeTriciDark}>Docs</Link>
+                </Stack.Item>
+              </Stack>
+          </Stack.Item>
             <Stack.Item>
               <a href="https://discord.gg/866ErbpvEF"><img src="images/join_discord.webp" alt="join us on discord" width="100px" /></a>
-            </Stack.Item>
-          </Stack>
+          </Stack.Item>
+          
+        </Stack>
 
 
 
